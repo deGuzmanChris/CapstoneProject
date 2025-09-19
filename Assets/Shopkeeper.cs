73a382e439c2416d34e26861c725ba0faf25ac85
Taze-Priizer <diem.salaysay@edu.sait.ca>
2025-09-19 00:43:03 -0600

    Fix the issue of MissingReferenceError
    I'll worry about the shop's mechanics with the coins later on since I'm having trouble fixing it. Time to start on creating some new enemies for the game

--- Assets/Shopkeeper.cs
+++ Assets/Shopkeeper.cs
@@ -1,45 +1,35 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Shopkeeper : MonoBehaviour
 {
-    public GameObject shopScreen; // Shop Screen UI panel will be dragged here
     private bool playerInRange = false;
 
-    
     void Update()
     {
-        // Shop screen UI pops up if player is in range and presses 'E'
-        if (playerInRange && !shopScreen.activeSelf && Input.GetKeyDown(KeyCode.E))
+        // Player presses E near the shopkeeper (no UI yet, just log)
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            shopScreen.SetActive(true); 
-        }
-
-        // Shop screen UI disappears if player presses 'Escape' while shop screen is still up
-        if (shopScreen.activeSelf && Input.GetKeyDown(KeyCode.Escape))
-        {
-            shopScreen.SetActive(false);
+            Debug.Log("Player is interacting with the shopkeeper!");
+            // Later: show shop UI here
         }
     }
 
-    //Triggers condition for accessing shop screen when player is close enough to the shop
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
+        {
             playerInRange = true;
+            Debug.Log("Player entered shop range");
+        }
     }
 
-    //Otherwise, condition doesn't trigger when player is too far from range
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-
-            // Shop screen closes automatically once player leaves range 
-            if (shopScreen.activeSelf)
-            {
-                shopScreen.SetActive(false);
-            }
+            Debug.Log("Player left shop range");
         }
     }
 }
+
